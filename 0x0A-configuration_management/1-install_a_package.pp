e1c92b4055799849df5c9d0a956cc8d7a2aa8010
Soukaina111 <basmahajar@gmail.com>
2024-03-22 09:02:47 -0700

    CRM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# This script install Flask version 2.1.0 usiing pip 3
+# This script  Install Flask using pip3
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
