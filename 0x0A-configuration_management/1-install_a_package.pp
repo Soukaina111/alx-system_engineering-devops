0ed77f0c4f015efb71e83ac4a4963a25965ea53b
Soukaina111 <basmahajar@gmail.com>
2024-03-22 08:49:27 -0700

    CRM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # This script install Flask version 2.1.0 using pip
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+exec { 'install_puppet_lint':
+  command => '/usr/bin/apt-get -y install puppet-lint=2.5.0',
 }
