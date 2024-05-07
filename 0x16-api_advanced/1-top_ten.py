f15eb71281bd3f5b6c5bcf802be8a38bd00ec5fc
Soukaina111 <basmahajar@gmail.com>
2024-05-07 08:49:26 -0700

    2SSS

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,21 +7,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """ Define the base URL for the Reddit API to do the job"""
+    """
+    Fetches and displays the titles of the top 10 hot posts from a
+    specified subreddit using the Reddit API
+    """
     base_url = "https://www.reddit.com/r/"
     endpoint = "hot.json"
     url = base_url + subreddit + endpoint
-    headers = {
-                "User-Agent": "ALXSE/0.0.1"
-                }
+    headers = {'User-Agent': "ALXSE/0.0.1"}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        hot_posts = data.get('data', {}).get('children', [])
-    if hot_posts:
-        for post in hot_posts[:10]:
+    try:
+        tt = response.json()['data']['children']
+        for post in tt:
             print(post['data']['title'])
-    else:
-        print("No hot posts found.")
-    else:
-        print(None)
+    except KeyError:
+        print("None")
