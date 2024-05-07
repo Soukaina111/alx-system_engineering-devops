e9b4fd0f30c853171bd088c25779edd854c63485
Soukaina111 <basmahajar@gmail.com>
2024-05-07 08:36:58 -0700

    2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,22 +7,21 @@ import requests
 
 
 def top_ten(subreddit):
-        """ Define the base URL for the Reddit API to do the job"""
-            base_url = "https://www.reddit.com/r/"
-                endpoint = "hot.json"
-                    url = base_url + subreddit + endpoint
-                        headers = {
-                                        "User-Agent": "ALXSE/0.0.1"
-                                                    }
-                            response = requests.get(url, headers=headers)
-                                if response.status_code == 200:
-                                            data = response.json()
-                                                    hot_posts = data.get('data', {}).get('children', [])
-                                                        if hot_posts:
-                                                                    for post in hot_posts[:10]:
-                                                                                    print(post['data']['title'])
-                                                                                        else:
-                                                                                                    print("No hot posts found.")
-                                                                                                        else:
-                                                                                                                    print(None)
-
+    """ Define the base URL for the Reddit API to do the job"""
+    base_url = "https://www.reddit.com/r/"
+    endpoint = "hot.json"
+    url = base_url + subreddit + endpoint
+    headers = {
+                "User-Agent": "ALXSE/0.0.1"
+                }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        hot_posts = data.get('data', {}).get('children', [])
+    if hot_posts:
+        for post in hot_posts[:10]:
+            print(post['data']['title'])
+    else:
+        print("No hot posts found.")
+    else:
+        print(None)
