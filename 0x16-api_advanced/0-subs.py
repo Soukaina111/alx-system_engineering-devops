5756153c2002d33f5647017f6c90f3309903ffd8
Soukaina111 <basmahajar@gmail.com>
2024-05-07 07:53:29 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+        # Define the base URL for the Reddit API
+            base_url = "https://www.reddit.com/r/"
+            # Define the endpoint for subreddit information
+            endpoint = "info.json"
+            # Construct the full URL
+            url = base_url + subreddit + endpoint
+            headers = {
+                        "User-Agent": "YourAppName/0.0.1"
+                    }
+            response = requests.get(url, headers=headers)
+                if response.status_code == 200:
+                            data = response.json()
+                            subscribers = data.get('data', {}).get('subscribers', 0)
+                            return subscribers
+                else:
+                            return 0
