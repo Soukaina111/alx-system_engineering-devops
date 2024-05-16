c2e7e02f8f8d4912d5ae22e0909551cb13aa89ca
Soukaina111 <basmahajar@gmail.com>
2024-05-16 07:52:50 -0700

    FIRST

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This script fixex the issue about the config files to have 0 failed requests
- exec { 'nginx fix':
-   onlyif   => 'test -e /etc/default/nginx',
-     command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
-       provider => 'shell'
-       }
+exec { 'nginx fix':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  provider => 'shell'
+}
